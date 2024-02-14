e1cb1c9e79caa0eca83bfded19ab8b9e466a51a4
Nemin32 <pergerlori@gmail.com>
2024-02-14 16:41:21 +0100

    Added room bounds checking.

--- adjacencies.ts
+++ adjacencies.ts
@@ -1,6 +1,6 @@
 import { rooms } from "./roomlist.ts"
 import { ROOM_NAME } from "./roomnames.ts"
-import { GM } from "./util.ts"
+import { GM, show } from "./util.ts"
 
 const adjacency: Map<string, boolean> = new Map()
 
@@ -13,15 +13,17 @@ const isAdjacent = (other: ROOM_NAME) => adjacency.get(`${ROOM_NAME[GM.currentNa
 
 export const move = (name: ROOM_NAME) => {
   if (name === ROOM_NAME.DEATH || GM.currentName === ROOM_NAME.DEATH || isAdjacent(name)) {
-    GM.prevName = GM.currentName
-    GM.currentName = name;
+    const newName = rooms.has(name) ? name : ROOM_NAME.SPAWN;
+    const newRoom = rooms.get(newName)!
 
-    const newRoom = rooms.get(name)
-    if (!newRoom) {
-      throw new Error("No such room as " + name)
+    if (name !== newName) {
+      show(`This is embarrassing. You tried to go to ${ROOM_NAME[name]}, but there is no such room. By the mysterious ways of Odd, you're placed back into the starting room. This was most likely caused by a bug. Please complain to Nemin about this.`)
     }
 
+    GM.prevName = GM.currentName
+    GM.currentName = name;
     GM.currentRoom = newRoom
+
     GM.currentRoom.printDescription()
   }
 }
