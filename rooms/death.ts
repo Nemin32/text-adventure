89665242c0cb8f40bb328d390ca748ee184cdad5
Nemin32 <pergerlori@gmail.com>
2024-02-15 13:02:37 +0100

    Fixed death count.

--- rooms/death.ts
+++ rooms/death.ts
@@ -14,10 +14,13 @@ const actions: ActionGenerator<flags> = (flags) => ({
   ]
 })
 
-const gotAllDeaths = GM.deaths.size < DEATH.__LENGTH
-const allDeathsMsg = "You've died in all possible ways.\nThe powers that be are very irritated by your bumbling, but also a little impressed as well by how naturally you seem to get yourself killed. How did you manage to survive until now? Regardless, you still have a place in their plans, so *go back* and change your fate."
-const normalDeathsMsg = `You've achieved ${GM.deaths.size} of ${DEATH.__LENGTH} possible deaths.\nHowever, the powers that be have other plans for you. They call for you to *go back* and change your fate.`
 
-const description = (flags: flags) => `[GAME OVER]\n${gotAllDeaths ? allDeathsMsg : normalDeathsMsg}`
+const description = (flags: flags) => {
+  const gotAllDeaths = GM.deaths.size >= DEATH.__LENGTH
+  const allDeathsMsg = "You've died in all possible ways.\nThe powers that be are very irritated by your bumbling, but also a little impressed as well by how naturally you seem to get yourself killed. How did you manage to survive until now? Regardless, you still have a place in their plans, so *go back* and change your fate."
+  const normalDeathsMsg = `You've achieved ${GM.deaths.size} of ${DEATH.__LENGTH} possible deaths.\nHowever, the powers that be have other plans for you. They call for you to *go back* and change your fate.`
+
+  return `[GAME OVER]\n${gotAllDeaths ? allDeathsMsg : normalDeathsMsg}`
+}
 
 export const death = new Room({}, actions, description)
