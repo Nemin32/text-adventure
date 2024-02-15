511decb6335ab8aa14c30b33cc807f3c213a4659
Nemin32 <pergerlori@gmail.com>
2024-02-15 19:15:05 +0100

    Added intro.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import { rooms } from "./roomlist.ts"
 import { ROOM_NAME } from "./roomnames.ts"
-import { GM } from "./util.ts"
+import { GM, show } from "./util.ts"
 
 GM.currentRoom = rooms.get(ROOM_NAME.SPAWN)!
 
@@ -19,4 +19,6 @@ window.addEventListener("load", () => {
     act(input.value.trim())
     input.value = ""
   })
+
+  show("You wake up with a splitting headache and your entire body in pain. You attempt to stand up, but your legs have seized. Half-blind you fumble with the harness until you find the small reset button.\nThe machine spends a few seconds just whirring helplessly while belching poisonous fumes. You're about to whack it in frustration when, without warning, it suddenly jerks back to life, allowing you to carefully stand back up.\nYou have vague memories about what happened, but still, it'd probably be the best to first *look around*. Unless of course you'd rather just call out to your momma for *help*.")
 })
