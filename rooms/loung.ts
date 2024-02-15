8940c179e0d6f5225b1fbee768c807a261a62226
Nemin32 <pergerlori@gmail.com>
2024-02-15 12:24:21 +0100

    Expanded lounge.

--- rooms/loung.ts
+++ rooms/loung.ts
@@ -1,7 +1,9 @@
+import { move } from "../adjacencies.ts";
 import { ActionGenerator, Flags, Room } from "../room.ts";
-import { show } from "../util.ts";
+import { ITEM, ROOM_NAME } from "../roomnames.ts";
+import { GM, show } from "../util.ts";
 
-type flags = Flags<never>
+type flags = Flags<"securityOpen">
 
 const actions: ActionGenerator<flags> = (flags) => ({
   look: [
@@ -15,20 +17,60 @@ const actions: ActionGenerator<flags> = (flags) => ({
     },
     {
       trigger: ["poster"],
-      action: () => show("")
+      action: () => show("The poster depicts a Slig proudly wearing the TreadMaster X500 Ultralight Kevlar-Composite Mechanical Harness, or - as you and the boys always called it - the good stuff. You always dreamt of one day being able to afford this model, but with the recent events the prospect seems more distant than ever.")
     },
     {
       trigger: ["TV", "tv", "television"],
-      action: () => show("")
+      action: () => show("The TV is repeating a pre-recorded message from The Magog on the March. Everything will be fine, just go back to work, let smarter people deal with it, yadda, yadda. I wonder if he even slightly believes the drivel they make him say.")
     },
 
     {
       trigger: ["door"],
-      action: () => show("")
+      action: () => flags.securityOpen 
+        ? show("The door leads into the security booth. With the padlock shot off, nothing stops you from just waltzing inside. Odd bless firearms.") 
+        : show("The door leads into the security booth. It's been haphazardly locked with a padlock, but as you inspect the mechanism, it seems very shoddy and weak. You're certain it wouldn't take much to blast it right off.")
     }
+  ],
+  use: [
+    {
+      trigger: ["door", "booth", "security booth"],
+      action: ({tool}) => {
+        if (flags.securityOpen) {
+          show("There is nothing else you need to do with the door.")
+          return;
+        }
+
+        if (tool === ITEM.GUN) {
+          if (GM.hasItem(ITEM.GUN)) {
+            show("It feels a bit wrong to waste your one bullet on something inanimate, but then this is an emergency. You take aim and blast the padlock into smithereens, unlocking the door in style.")
+            flags.securityOpen = true;
+          } else {
+            show("Yeah, good idea, if only I had a gun to actually try it.")
+          }
+        } else {
+          show("No, I don't think that'd work.")
+        }
+      }
+    }
+  ],
+  enter: [
+    {
+      trigger: ["path", "boiler room"],
+      action: () => move(ROOM_NAME.BOILA)
+    },
+    {
+      trigger: ["door", "security", "security booth"],
+      action: () => {
+        if (flags.securityOpen) {
+          move(ROOM_NAME.SECUR)
+        } else {
+          show("You try to enter the booth, but it is locked.")
+        }
+      }
+    },
   ]
 })
 
-const description = (flags: flags) => `You step into the employee lounge area. You've never quite understood why they placed the lounge past the boilers, but then you've suspected for years that RuptureFarms wasn't exactly built up to code... If there even was a code to begin with.\nStill, even with all the chaos and destruction outside, you feel a bit of fuzziness in your cold, dead heart as you gaze over the place that gave you so many good memories. A poker *table* dominates the middle of the room, situated between a handful of three-legged chairs. The floor is covered by a cheap, faded carpet, its pattern long unrecognizable under all the stains and years of abuse. A *darts board* hangs on the far end of the wall, next to it a faded *poster*. Below them, on a small cubby, you see an old *TV*. As you make your way through the room, your legs keep kicking away empty bottles of SoulStorm Brew. You smile. It's a mess for sure, but it was always your mess.\nA *door* to the left leads into a nearby security booth, while the *path* back into the boiler room is behind you.`
+const description = (flags: flags) => `You step into the employee lounge area. You've never quite understood why they placed the lounge past the boilers, but then you've suspected for years that RuptureFarms wasn't exactly built up to code... If there even was a code to begin with.\nStill, even with all the chaos and destruction outside, you feel a bit of fuzziness in your cold, dead heart as you gaze over the place that gave you so many good memories. A poker *table* dominates the middle of the room, situated between a handful of three-legged chairs. The floor is covered by a cheap, faded carpet, its pattern long unrecognizable under all the stains and years of abuse. A *darts board* hangs on the far end of the wall, next to it a faded *poster*. Below them, on a small cubby, you hear an old *TV* babbling to itself. As you make your way through the room, your legs keep kicking away empty bottles of SoulStorm Brew. You smile. It's a mess for sure, but it was always your mess.\nA *door* to the left leads into a nearby security booth, while the *path* back into the boiler room is behind you.`
 
-export const loung = new Room({}, actions, description)
+export const loung = new Room({securityOpen: false}, actions, description)
